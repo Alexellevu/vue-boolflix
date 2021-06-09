00dd8dc5428707630dfce7683ce7bd147db9354c
alexellevu <newnapster83@gmail.com>
2021-06-09 15:06:42 +0200

    ristrutturata parte vuejs

--- assets/main.js
+++ assets/main.js
@@ -2,29 +2,35 @@ const app = new Vue({
     el:'#app',
 
     data:{
-        url:'https://api.themoviedb.org/3/search/movie',
+        url:'https://api.themoviedb.org/3/search/movie?',
         apiKey:'baafb21ea0b84e839d86a829c7a751c1',
-        query:''
+        query:'',
+        error:''
     },
 
     methods:{
+        searchMovies(query)  {
+            console.log(query);
+            const fullUrl = this.url +'api_key='+ this.apiKey +'&query='+ query;
+     
+
+             console.log(fullUrl);
+ 
+            axios
+            .get(fullUrl)
+            .then(response => {
+            console.log(response);
+            })
+            
+            .catch(e => {
+                console.error(e);
+                this.error ='non è possibile connettersi all API'
+            })
+        }
     },
 
     mounthed(){
-        var fullUrl =`${this.url}?api_key=${this.apikey}&query=${this.query}`;
-     
-
-        console.log(fullUrl);
-       axios
-        .get(fullUrl)
-        .then(response => {
-            console.log(response);
-            
-        })
-        .catch(e => {
-            console.error(e);
-            this.error ='non è possibile connettersi all API'
-        })
+       
     }
 
 })
