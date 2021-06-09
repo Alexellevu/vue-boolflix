02a6ab32e554ee71e9204f404188ebb18ad2d764
alexellevu <newnapster83@gmail.com>
2021-06-09 09:34:16 +0200

    aggiuta query dinamica

--- assets/main.js
+++ assets/main.js
@@ -2,23 +2,24 @@ const app = new Vue({
     el:'#app',
 
     data:{
-        url:'https://developers.themoviedb.org/3/search/search-movies',
-        query:'',
-        apiKey:'baafb21ea0b84e839d86a829c7a751c1'
-        
+        url:'https://api.themoviedb.org/3/search/movie',
+        apiKey:'baafb21ea0b84e839d86a829c7a751c1',
+        query:''
     },
 
     methods:{
-
     },
 
     mounthed(){
-        
+        var fullUrl =`${this.url}?api_key=${this.apikey}&query=${this.query}`;
+     
+
+        console.log(fullUrl);
        axios
-        .get(this.url)
+        .get(fullUrl)
         .then(response => {
-            
             console.log(response);
+            
         })
         .catch(e => {
             console.error(e);
