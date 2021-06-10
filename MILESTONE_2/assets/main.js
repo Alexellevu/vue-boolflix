c199716382c64c2feedc84dff47005fac087f6c2
alexellevu <newnapster83@gmail.com>
2021-06-10 09:55:13 +0200

    agiunta ricerca serie tv

--- MILESTONE_2/assets/main.js
+++ MILESTONE_2/assets/main.js
@@ -3,10 +3,12 @@ const app = new Vue({
 
     data:{
         url:'https://api.themoviedb.org/3/search/movie?',
+        url2:'https://api.themoviedb.org/3/search/tv?',
         apiKey:'baafb21ea0b84e839d86a829c7a751c1',
         query:'',
         error:'',
         movies:'',
+        tvSeries:'',
         voti:[],
         vuote:[]
         
@@ -23,6 +25,30 @@ const app = new Vue({
             .then(response => {
             /* console.log(response); */
             this.movies=response.data.results;
+           /*  console.log(this.movies); */
+            for(var i=0; i<response.data.results.length; i++){
+            this.voti.push(Math.ceil(response.data.results[i].vote_average/2));
+            this.vuote.push(Math.ceil(4-[response.data.results[i].vote_average/2]))
+            }
+          /*   console.log(this.voti); */
+          console.log(this.vuote);
+           })
+            
+            .catch(e => {
+                console.error(e);
+                this.error ='non è possibile connettersi all API'
+            })
+        },
+
+        searchTv(query){
+            const fullUrl2 = this.url2 +'api_key='+ this.apiKey +'&query='+ this.query;
+            /* console.log(fullUrl); */
+ 
+            axios
+            .get(fullUrl2)
+            .then(response => {
+            console.log(response);
+            this.tvSeries=response.data.results;
            /*  console.log(this.movies); */
             for(var i=0; i<response.data.results.length; i++){
             this.voti.push(Math.ceil(response.data.results[i].vote_average/2));
